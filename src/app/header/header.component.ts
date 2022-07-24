4b86d6ab0b758630691e8bf59272e2904c30ffc1
Florent <109805542+FlorentGargot@users.noreply.github.com>
2022-07-24 21:43:41 +0200

    menu desktop adjustments

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   isMobileLayout = false;
+  isDesktopLayout = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    window.onresize = () => this.isMobileLayout = window.innerWidth < 768;
+    window.onload = () => {
+      this.isMobileLayout = window.innerWidth < 768;
+      this.isDesktopLayout = window.innerWidth >= 1024
+    };
+    window.onresize = () => {
+      this.isMobileLayout = window.innerWidth < 768;
+      this.isDesktopLayout = window.innerWidth >= 1024
+    };
+    
+  }
+
+  onOpenMenuClick(){
+    const menuitem = document.getElementById("menu");
+    menuitem?.classList.add('opened');
+    
+  }
+
+  onCloseMenuClick(){
+    const menuitem = document.getElementById("menu");
+    menuitem?.classList.remove('opened');
+
   }
 
 }
